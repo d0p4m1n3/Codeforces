b4b9c1f5eec223a6a596b06f49e75a31a2998505
kujo <80rN70c0D3@protonmail.com>
2020-09-11 21:15:14 +0200

    upload

---
+++ petya_and_strings.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <strings.h>
+
+int main()
+{
+	char a[100], b[100], x;
+	scanf("%s %s", &a, &b);
+	x = strcasecmp(a, b);
+	if (x == 0)
+		printf("0\n");
+	else if (x < 0)
+		printf("-1\n");
+	else
+		printf("1\n");
+	
+	return 0;
+}
